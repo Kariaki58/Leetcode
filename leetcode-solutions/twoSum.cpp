adca37cc7846858f8e3e51afc7692da5f670e981
kariaki58 <kariakistephen809@gmail.com>
2024-10-09 18:11:49 +0100

    imported c++ libarys

--- leetcode-solutions/twoSum.cpp
+++ leetcode-solutions/twoSum.cpp
@@ -1,7 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> numMap;
+        unordered_map<int, int> numMap;
 
         for (int i = 0; i < nums.size(); i++) {
             int validate = target - nums[i];
