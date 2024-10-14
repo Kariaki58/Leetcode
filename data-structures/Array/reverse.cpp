0c17a9cd4140455fa89ec5329d36e74e57beefe5
kariaki58 <kariakistephen809@gmail.com>
2024-10-14 13:28:28 +0100

    reversing array using inbuilt function in c++

--- data-structures/Array/reverse.cpp
+++ data-structures/Array/reverse.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+
 using namespace std;
 
+
+void reserveArrayInBuiltAlgorithm(vector<int> &arr) {
+    reverse(arr.begin(), arr.end());
+}
 
 void reverseArray(vector<int> &arr) {
     int n = arr.size();
@@ -36,9 +43,21 @@ void reverseArraySwap(vector<int>& arr) {
     }
 }
 
+void reverseArrayRec(vector<int>& arr, int l, int r) {  
+    if (l >= r)
+        return;
+    swap(arr[l], arr[r]);
+    reverseArrayRec(arr, l + 1, r - 1);
+}
+
+void reverseArrayRecursive(vector<int> &arr) {
+    int n = arr.size();
+    reverseArrayRec(arr, 0, n - 1);
+}
+
 int main() {
     vector<int> arr = { 1, 4, 3, 2, 6, 5 };
-    reverseArraySwap(arr);
+    reverseArrayRecursive(arr);
 
     for (int i = 0; i < arr.size(); i++)
         cout << arr[i] << " ";
